3500e61b2d18850d5358ff517b2f74d0d90c7100
WodaPL <wodapl@gmail.com>
2022-08-22 21:14:58 +0200

    Background video scroll applied

--- libs/core/ui/sections/src/lib/components/showcase-points-video/showcase-points-video.tsx
+++ libs/core/ui/sections/src/lib/components/showcase-points-video/showcase-points-video.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react';
 import {
   Wrapper,
   ContextContainer,
@@ -5,6 +6,7 @@ import {
   Subtitle,
   BackgroundVideo,
 } from '../../styles/showcase-points-video/showcase-points-video';
+import { Controller, Scene } from 'react-scrollmagic';
 
 import ShowcasePointsVideoPoints from './showcase-points-video-points';
 import ShowcasePointsVideoResults from './showcase-points-video-results';
@@ -20,17 +22,48 @@ export const ShowcasePointsVideo = ({
   id?: string;
   className?: string;
 }) => {
+  //Store
+  const [progressValue, setProgressValue] = useState(0);
+
+  //Refs
+  const backgroundVideo = useRef<HTMLVideoElement>(null);
+
+  //Others
+  let videoDuration = 7.8;
+  let pixelsScroll = 5000;
+  let playRatio = pixelsScroll / videoDuration / 100;
+
+  useEffect(() => {
+    if (backgroundVideo.current) {
+      backgroundVideo.current.currentTime = progressValue;
+    }
+  }, [progressValue]);
+
   return (
-    <Wrapper id={id}>
-      <ContextContainer>
-        <Logo src={logoUrl} />
-        <Subtitle>Kanał kulinarny o świrowaniu</Subtitle>
-        <ShowcasePointsVideoPoints />
-        <ShowcasePointsVideoResults />
-        <ShowcasePointsVideoSocialLinks />
-      </ContextContainer>
-      <BackgroundVideo src={backgroundVideoUrl} muted autoPlay />
-    </Wrapper>
+    <Controller>
+      <Scene duration={pixelsScroll} pin={true} reverse={true} triggerHook={0}>
+        {(progress: number) => {
+          setProgressValue(progress * playRatio);
+
+          return (
+            <Wrapper id={id}>
+              <ContextContainer>
+                <Logo src={logoUrl} />
+                <Subtitle>Kanał kulinarny o świrowaniu</Subtitle>
+                <ShowcasePointsVideoPoints />
+                <ShowcasePointsVideoResults />
+                <ShowcasePointsVideoSocialLinks />
+              </ContextContainer>
+              <BackgroundVideo
+                muted
+                ref={backgroundVideo}
+                src={backgroundVideoUrl}
+              />
+            </Wrapper>
+          );
+        }}
+      </Scene>
+    </Controller>
   );
 };
 
